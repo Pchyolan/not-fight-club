829d79f2c61294fc41c5f3db7372fab904f5d435
unknown <mozhno_prost_kostya@mail.ru>
2025-08-17 17:43:47 +0300

    init: create player registration page (html, css, js)

---
+++ registr.js
@@ -0,0 +1,11 @@
+const input = document.getElementById("inputName");
+const button = document.getElementById("btnSave");
+
+button.addEventListener("click", () => {
+    const name = input.value.trim();
+    if (!name) return;
+
+    
+    localStorage.setItem("playerName", name);
+    window.location.href = "index.html";
+});
