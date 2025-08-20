66c7fef8ce9b9892b289440441e4e0a9354cba46
unknown <mozhno_prost_kostya@mail.ru>
2025-08-21 02:56:49 +0300

    feat: update fight-button

--- boy.js
+++ boy.js
@@ -22,3 +22,107 @@ const playerIcon = localStorage.getItem("choseIcon");
 persName.textContent = playerName;
 document.getElementById("photo-fight").src = playerPhoto;
 document.getElementById("pers-icon-fight").src = playerIcon;
+
+
+const btnsAttack = document.querySelectorAll(".attack-btn");
+const btnFight = document.getElementById("fight-btn");
+let choseAttack = null;
+const btnsDefens = document.querySelectorAll(".defens-btn");
+let choseDefensBtn = [];
+
+
+function checkAttack() {
+    const isRight = choseAttack !== null && choseDefensBtn.length == 2;
+    btnFight.disabled = !isRight;
+}
+
+function choseBtn(chose) {
+    btnsAttack.forEach(btn => btn.classList.remove('selected'));
+    chose.classList.add('selected');
+    choseAttack = chose.dataset.zone;
+    checkAttack();
+}
+
+btnsAttack.forEach(btn => {
+    btn.addEventListener("click", () => {
+        choseBtn(btn);
+    });
+});
+
+function goLight() {
+    btnsDefens.forEach(btn => {
+        const zone = btn.dataset.zone;
+        if (choseDefensBtn.includes(zone)) {
+            btn.classList.add('selected');
+        } else {
+            btn.classList.remove('selected');
+        }
+    });
+}
+
+btnsDefens.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const zone = btn.dataset.zone;
+        if (choseDefensBtn.includes(zone)) {
+            choseDefensBtn = choseDefensBtn.filter(n => n !== zone);
+        } else {
+            if (choseDefensBtn.length >= 2) {
+                choseDefensBtn.shift();
+            }
+            choseDefensBtn.push(zone);
+        }
+        goLight();
+        checkAttack()
+    });
+});
+
+goLight();
+
+const player = {
+    name: playerName,
+    hp: 100,
+    maxHp: 100,
+    damage: 10,
+    critChance: 0.2,
+    critCoeff: 1.5,
+    attacksPerTurn: 1,
+    blocksPerTurn: 2
+}
+
+const opponents = [
+    {
+        name: "Лао Инь",
+        img: "img/prot-1.png",
+        hp: 100,
+        maxHp: 100,
+        damage: 10,
+        critChance: 0.2,
+        critCoeff: 1.5,
+        attacksPerTurn: 2,
+        blocksPerTurn: 1
+    },
+    {
+        name: "Мясник",
+        img: "img/prot-2.png",
+        hp: 100,
+        maxHp: 100,
+        damage: 20,
+        critChance: 0.2,
+        critCoeff: 1.5,
+        attacksPerTurn: 1,
+        blocksPerTurn: 3
+    },
+    {
+        name: "Шиз",
+        img: "img/prot-3.png",
+        hp: 100,
+        maxHp: 100,
+        damage: 15,
+        critChance: 0.2,
+        critCoeff: 1.5,
+        attacksPerTurn: 1,
+        blocksPerTurn: 1
+    },
+]
+
+const randomOpp = opponents[Math.floor(Math.random * opponents.length)]
